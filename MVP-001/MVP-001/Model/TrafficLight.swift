71708fa2eb1025a685b462dedbd72a8080fbc49a
omrobbie <putu.roby@yahoo.com>
2020-08-14 10:29:18 +0800

    create data model

---
+++ MVP-001/MVP-001/Model/TrafficLight.swift
@@ -0,0 +1,13 @@
+//
+//  TrafficLight.swift
+//  MVP-001
+//
+//  Created by omrobbie on 14/08/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+struct TrafficLight {
+
+    let colorName: String
+    let description: String
+}
